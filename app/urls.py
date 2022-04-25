23c6db58bee52ff1c9aa11a72c972b7f3d61955d
s.khosravi <khosravipour.salman@gmail.com>
2022-04-25 15:20:19 +0430

    add create password object

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app import views
 urlpatterns = [
     path('list/', views.password_list, name='list'),
     path('detail/<int:pk>/', views.password_detail, name='detail'),
+    path('create/', views.password_create, name='create'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.models import PasswordModel
 from django.shortcuts import get_object_or_404
+
 
 def password_list(request):
     object_list = PasswordModel.objects.all()
@@ -16,3 +17,15 @@ def password_detail(request, pk):
         'obj': get_object_or_404(PasswordModel, id=pk),
     }
     return render(request, 'app/detail.html', context)
+
+
+def password_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        username = request.POST['username']
+        password = request.POST['password']
+
+        PasswordModel.objects.create(title=title, username=username, password=password)
+        return redirect('list')
+
+    return render(request, 'app/create.html')
